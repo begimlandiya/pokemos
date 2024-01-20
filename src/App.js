07f57f396a08bea33f93acf649afec99ee37aec6
begimlandiya <erkinovabegimai05@gmail.com>
2024-01-20 17:17:51 +0600

    tasks are done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const API = "https://pokeapi.co/api/v2/pokemon?limit=10";
+const Pokemon = () => {
+  const [allPokemons, setAllPokemons] = useState([]);
+
+  const getAllPokemons = async () => {
+    try {
+      const response = await axios.get(`${API}`);
+      console.log(response.data.results);
+
+      const getPokemonDetails = response.data.results.map(async (pokemon) => {
+        const response2 = await axios(pokemon.url);
+        console.log(response2);
+        return response2.data;
+      });
+
+      const results = await Promise.all(getPokemonDetails);
+
+      setAllPokemons(results);
+    } catch (error) {
+      console.error("Ошибка, не удалось получить данные!", error);
+    }
+  };
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="h1">Pokemon Cards</h1>
+      <div className="pokemon-container">
+        {allPokemons.map((pokemon, index) => (
+          <figure className="pokemon-card" key={index}>
+            <figcaption>Name: {pokemon.name}</figcaption>
+            <figcaption>Weight: {pokemon.weight}</figcaption>
+            <figcaption>
+              Types: {pokemon.types.map((type) => type.type.name).join(", ")}
+            </figcaption>
+            <img src={pokemon.sprites.front_default} />
+          </figure>
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div>
+      <Pokemon />
+    </div>
+  );
+};
 
 export default App;
